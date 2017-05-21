602b3a21aec70af6ef6775a897351a95c196ff4e
Taylor Hansen <PeterPan@Taylors-MacBook-Pro-9.local>
2017-05-20 21:37:06 -0400

    Initial Commit

---
+++ Photo Zen Final Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo Zen Final Project
+//
+//  Created by Taylor Hansen on 20/05/2017.
+//  Copyright © 2017 Taylor Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
